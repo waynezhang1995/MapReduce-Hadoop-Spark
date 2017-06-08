daaa0fb89f8fd3dffad5d38907dae29d29941cd7
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-06-08 14:10:09 -0700

    gitignore added

--- hadoop_examples_post/src/MovielensGenreAvg.java
+++ hadoop_examples_post/src/MovielensGenreAvg.java
@@ -14,55 +14,55 @@ import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.BasicConfigurator;
 
 /*This class is responsible for running map reduce job*/
-//Change a path as indicated below. 
+//Change a path as indicated below.
 public class MovielensGenreAvg extends Configured implements Tool
 {
-	
+
 	public int run(String[] args) throws Exception {
 		BasicConfigurator.configure();
 		//Logger.getRootLogger().setLevel(Level.ERROR);
-		
+
 		args = new String[] {"input_movielens", "output_movielens"};
-		
+
 		if(args.length !=2) {
 			System.err.println("Usage: MovielensAvg <input path> <output path>");
 			System.exit(-1);
 		}
-		
+
 		System.out.println("See output in folder: " + args[1]);
-		
+
 		//remove output directory if already there
 		FileSystem fs = FileSystem.get(getConf());
 		fs.delete(new Path(args[1]), true); // delete file, true for recursive
-		
-		Job job = Job.getInstance(getConf()); 
+
+		Job job = Job.getInstance(getConf());
 		job.setJarByClass(MovielensGenreAvg.class);
 		job.setJobName("Movielens Genre Avg");
-		
+
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileInputFormat.setInputDirRecursive(job, true); //To read recursively all sub-directories, not set by default
 		FileOutputFormat.setOutputPath(job,new Path(args[1]));
-		
+
 		job.setMapperClass(MovielensGenreAvgMapper.class);
 		job.setReducerClass(MovielensGenreAvgReducer.class);
 		job.setCombinerClass(MovielensGenreAvgCombiner.class);
-		
+
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(MapWritable.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(DoubleWritable.class);
-		
+
 		//We assume that the movies.csv file fits in memory.
-		//This call will save the file locally in each machine in the cluster. 
+		//This call will save the file locally in each machine in the cluster.
 		//CHANGE THIS PATH TO YOUR SPECIFIC LOCATION
-		job.addCacheFile(new URI("file:///C:/Users/Alex/workspace_neon/hadoop_examples/movielens/ml-latest-small/movies.csv"));
+		job.addCacheFile(new URI("/Users/waynezhang/Desktop/Dev/MapReduce-Hadoop-Spark/hadoop_examples_post/movielens/ml-latest-small/movies.csv"));
 
 		long startTime = System.currentTimeMillis();
-		
+
 		boolean success = job.waitForCompletion(true);
-		
+
 		System.out.println("Time elapsed (sec) = " + (System.currentTimeMillis() - startTime) / 1000.0);
-		
+
 		return success ? 0 : 1;
 	}
 
